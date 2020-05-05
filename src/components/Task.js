e30bf835379f6136a7414eafa7ad245b03689e4f
Rob Brennan <rob@therobbrennan.com>
2020-05-05 15:03:58 -0700

    Implement HTML of the Task component to match our design

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
-// Basic implementation of the Task, simply taking in the attributes we know we’ll need and the two actions you can take on a task (to move it between lists)
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          defaultChecked={state === 'TASK_ARCHIVED'}
+          disabled={true}
+          name="checked"
+        />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+      </div>
+
+      <div className="actions" onClick={event => event.stopPropagation()}>
+        {state !== 'TASK_ARCHIVED' && (
+           // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a onClick={() => onPinTask(id)}>
+            <span className={`icon-star`} />
+          </a>
+        )}
+      </div>
     </div>
   );
 }
